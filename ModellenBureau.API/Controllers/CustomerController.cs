a3e482d4ea442761c5d7d49b77d984920aa24991
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-02-04 15:35:01 +0100

    customercontroller

---
+++ ModellenBureau.API/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModellenBureau.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModellenBureau.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : Controller
+    {
+        private readonly ICustomerRepository _customerRepository;
+
+        public CustomerController(ICustomerRepository customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+    }
+}
