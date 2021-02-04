077e18ac0e80b280711fce5e252a6dc0c13d27fe
Luna Herder <luna@herderfactory.nl>
2021-02-04 14:20:09 +0100

    PhotoModelRepository

--- ModellenBureau.API/Models/IPhotoModelRepository.cs
+++ ModellenBureau.API/Models/IPhotoModelRepository.cs
@@ -1,4 +1,4 @@
-﻿using ModellenBureau.Main.Server.Models;
+﻿using ModellenBureau.Main.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +9,9 @@ namespace ModellenBureau.API.Models
     public interface IPhotoModelRepository
     {
         IEnumerable<PhotoModel> GetAllModels();
-        PhotoModel GetModelById(int modelId);
-        PhotoModel AddModel(int modelId);
-        PhotoModel UpdateModel(int modelId);
-        void DeleteModel(int modelId);
+        PhotoModel GetModelById(int photoModelId);
+        PhotoModel AddModel(PhotoModel photoModel);
+        PhotoModel UpdateModel(PhotoModel photoModel);
+        void DeleteModel(int photoModelId);
     }
 }
