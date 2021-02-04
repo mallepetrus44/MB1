c347500d99b024b52c1dede48ed55a9451d68022
Luna Herder <luna@herderfactory.nl>
2021-02-04 13:13:24 +0100

    Customer model aangepast

--- ModellenBureau.Shared/Customer.cs
+++ ModellenBureau.Shared/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,14 @@ namespace ModellenBureau.Main.Server.Models
 {
     public class Customer : User
     {
+        [Required]
         public string Logo { get; set; }
 
+        [Required]
+        [MaxLength(8), MinLength(8)]
         public int KvkNummer { get; set; }
 
+        [Required]
         public string BtwNummer { get; set; }
     }
 }
