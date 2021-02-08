7d4acbc63953ac1cb37cb08c5e3688791184b6a1
Mathijs van de Kerkhof <mathijs.vandekerkhof@itvitaelearning.nl>
2021-02-08 14:08:46 +0100

    customerDetails

---
+++ ModellenBureau.Server/Pages/CustomerDetailBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using ModellenBureau.Main.Shared;
+using ModellenBureau.Server.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModellenBureau.Server.Pages
+{
+    public class CustomerDetailBase : ComponentBase
+    {
+        [Inject]
+        public ICustomerDataService CustomerDataService { get; set; }
+
+        [Parameter]
+        public string CustomerId { get; set; }
+
+        public Customer Customer { get; set; } = new Customer();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Customer = await CustomerDataService.GetCustomerDetails(int.Parse(CustomerId));
+
+        }
+    }
+}
