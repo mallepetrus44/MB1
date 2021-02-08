8c9e4a27086d4e48ae588b472f9c1efaf33b89ad
Luna Herder <luna@herderfactory.nl>
2021-02-08 15:28:46 +0100

    PhotoModelDetail page

---
+++ ModellenBureau.Server/Pages/PhotoModelDetailBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using ModellenBureau.Main.Shared;
+using ModellenBureau.Server.Services;
+
+namespace ModellenBureau.Server.Pages
+{
+    public class PhotoModelDetailBase : ComponentBase
+    {
+        [Inject]
+        public IPhotoModelDataService PhotoModelDataService { get; set; }
+
+        [Parameter]
+        public string PhotoModelId { get; set; }
+
+        public PhotoModel PhotoModel { get; set; } = new PhotoModel();
+
+        protected override async Task OnInitializedAsync()
+        {
+            PhotoModel = await PhotoModelDataService.GetPhotoModelDetails(int.Parse(PhotoModelId));
+        }
+    }
+}
