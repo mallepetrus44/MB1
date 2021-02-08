e6e03877482bca08bca4de4fd370aaa15a43cd40
Luna Herder <luna@herderfactory.nl>
2021-02-08 13:33:38 +0100

    added PhotoModelDataService and IPhotoModelDataService

---
+++ ModellenBureau.Server/Services/IPhotoModelDataService.cs
@@ -0,0 +1,17 @@
+﻿using ModellenBureau.Main.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModellenBureau.Server.Services
+{
+    public interface IPhotoModelDataService
+    {
+        Task<IEnumerable<PhotoModel>> GetAllPhotoModels();
+        Task<PhotoModel> GetPhotoModelDetails(int photoModelId);
+        Task<PhotoModel> AddPhotoModel(PhotoModel photoModel);
+        Task UpdatePhotoModel(PhotoModel photoModel);
+        Task DeletePhotoModel(int photoModelId);
+    }
+}
